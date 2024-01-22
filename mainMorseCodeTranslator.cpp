e36f5141b2fd57b4d7ca025b8fef60f865d2aab5
Erik Westerstråhle <111179152+Erik-Westerstrahle@users.noreply.github.com>
2024-01-22 08:29:38 +0000

    added more

--- mainMorseCodeTranslator.cpp
+++ mainMorseCodeTranslator.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  
+}
+class MainMorseCodeTranslator {
+private:
+    std::unordered_map<char, std::string> morseCodeMap;
+
+
+public:
+MainMorseCodeTranslator(){
+        // Initialize the Morse code map
+        morseCodeMap = {
+            {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+            {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+            {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+            {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+            {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+            {'Z', "--.."}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"},
+            {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."},
+            {'8', "---.."}, {'9', "----."}, {'0', "-----"}, {' ', " "}
+        };
+
+        std:string translateToMorse(const std::string& text) {
+            std::string morse ="";
+            for(char c : text) {
+                c =toupper(c);// Convert to uppercase for consistent mapping
+
+                if(morseCodeMap.find(c) != morseCodeMap.end())
+                {
+                    morse += morseCodeMap(c) + " ";
+
+                }
+                else{
+                    morse += "? ";
+                }
+            }
+            return morse;
+        }
+};
+
+
 }
